6cd8090e1a78eef6aac40de295d7aeabfae9ad83
Marcello Seri <marcello.seri@gmail.com>
2015-06-30 14:24:34 +0100

    Added pixellisation logic

--- tweetpix.py
+++ tweetpix.py
@@ -1,17 +1,24 @@
+import json
 import tweepy
-from requests import get
-from json import json
-from pixellize import pixellize
-from random import choice, randint
+
+from io import BytesIO
 from keys import keys
+from PIL import Image
+from pixellize import pixellize, scaled_size
+from random import choice, randint
+from requests import get
+
 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
-FLICKR_API_KEY = keys['flickr_key']
 
+FLICKR_API_KEY = keys['flickr_key']
+# https://www.flickr.com/services/api/flickr.photos.search.html
 FLICKR_SEARCH_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={}&text={}&license=1%2C2%2C9%2C10&sort=interestingness-desc&media=photos&format=json&nojsoncallback=1"
+# https://www.flickr.com/services/api/misc.urls.html
+FLICKR_PHOTO_URL = "https://farm{farm}.staticflickr.com/{server}/{id}_{secret}_h.jpg"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -22,8 +29,16 @@ def get_random_image_with(topic):
     raw_data = get(search_url)
     decoded_data = str(raw_data.content, encoding="utf-8", errors="ignore")
     json_data = json.loads(decoded_data)
-    random_picture = choice(json_data['photos']['photo'])
-    
+    rpic = choice(json_data['photos']['photo'])
+    photo_url = FLICKR_PHOTO_URL.format(**rpic)
+
+    image_data = get(photo_url).content
+    stream = BytesIO(image_data)
+    image = Image.open(stream).convert("RGB")
+
+    uniform_size = scaled_size(1600 / max(image.size), image.size)
+    pixellated = pixellize(image.resize(uniform_size))
+    pixellated.convert("RGB").save("test.png")
     pass
 
 def get_random_parameters():
