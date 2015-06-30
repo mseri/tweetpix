84c927c2df263435d4821829b0c4fdfee76771be
Marcello Seri <marcello.seri@gmail.com>
2015-06-30 16:37:29 +0100

    Added image generation

--- tweetpix.py
+++ tweetpix.py
@@ -18,41 +18,51 @@ FLICKR_API_KEY = keys['flickr_key']
 # https://www.flickr.com/services/api/flickr.photos.search.html
 FLICKR_SEARCH_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={}&text={}&license=1%2C2%2C9%2C10&sort=interestingness-desc&media=photos&format=json&nojsoncallback=1"
 # https://www.flickr.com/services/api/misc.urls.html
-FLICKR_PHOTO_URL = "https://farm{farm}.staticflickr.com/{server}/{id}_{secret}_h.jpg"
+FLICKR_PHOTO_URL = "farm{farm}.staticflickr.com/{server}/{id}_{secret}_b.jpg"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-def get_random_image_with(topic):
+
+def generate_url_for(topic):
     search_url = FLICKR_SEARCH_URL.format(FLICKR_API_KEY, topic)
     raw_data = get(search_url)
     decoded_data = str(raw_data.content, encoding="utf-8", errors="ignore")
     json_data = json.loads(decoded_data)
     rpic = choice(json_data['photos']['photo'])
-    photo_url = FLICKR_PHOTO_URL.format(**rpic)
 
-    image_data = get(photo_url).content
+    return FLICKR_PHOTO_URL.format(**rpic)
+
+
+def get_random_image_from(photo_url):
+
+    image_data = get("https://" + photo_url).content
     stream = BytesIO(image_data)
     image = Image.open(stream).convert("RGB")
 
-    uniform_size = scaled_size(1600 / max(image.size), image.size)
-    pixellated = pixellize(image.resize(uniform_size))
-    pixellated.convert("RGB").save("test.png")
-    pass
+    return image
+
 
 def get_random_parameters():
-    randint()
+    randint(1, 2)
     pass
+
 
 def prepare_image(image):
-    pixellize(image)
-    pass
+    uniform_size = scaled_size(1600 / max(image.size), image.size)
+    pixellated = pixellize(image.resize(uniform_size)).convert("RGB")
+
+    return pixellated
+
 
 def publish_image(image):
-    image
-    keys
     pass
 
+
 if __name__=="__main__":
-    pass
+    photo_url = generate_url_for("panorama")
+    image = get_random_image_from(photo_url)
+    pixellatedimage = prepare_image(image)
+
+    pixellatedimage.save(photo_url.replace("/", "."))
